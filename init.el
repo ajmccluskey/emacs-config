d5bb78b72b65875c02dab46f98bed268be083373
Andrew McCluskey <andrew@ajmccluskey.com>
2013-03-15 20:08:55 +1100

    Initial commit for emacs settings

---
+++ init.el
@@ -0,0 +1,10 @@
+;; Setup our load path and load our custom init stuff
+(add-to-list 'load-path "~/.emacs.d/init/")
+(load-library "my-packages")
+
+;; Highlight matching parens
+(show-paren-mode t)
+;; Show line numbers in margin
+(global-linum-mode t)
+;; Show column numbers in mode line
+(column-number-mode t)
---
+++ init/my-packages.el
@@ -0,0 +1,32 @@
+;; Pull in Marmalade packages
+(require 'package)
+(add-to-list 'package-archives 
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;; Ensure our preferred packages are all loaded in this install - taken from
+;; http://batsov.com/articles/2012/02/19/package-management-in-emacs-the-good-the-bad-and-the-ugly/
+(defvar my-packages
+  '(clojure-mode
+    clojure-test-mode
+    paredit
+    nrepl)
+  "A list of packages to ensure are installed at launch.")
+
+(defun my-packages-installed-p ()
+  (loop for p in my-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (my-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p my-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(provide 'my-packages)
