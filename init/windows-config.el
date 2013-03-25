8905e914cccc91005b40b81d1b4da1037dfd1482
Andrew McCluskey <andrew@ajmccluskey.com>
2013-03-25 11:31:41 +1100

    Add windows specific config
    Currently all we need to do for Windows is set the default font.

---
+++ init/windows-config.el
@@ -0,0 +1,6 @@
+;; Configuration options specific to Windows
+
+(custom-set-faces
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
+
+(provide 'windows-config)
