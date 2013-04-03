522869d12e414fc59f4f3aac4b6d84411c0d0d8a
Andrew McCluskey <andrew@ajmccluskey.com>
2013-04-03 14:42:45 +1100

    Add logic to pick first available font in Windows
    XP doesn't have our preferred font, so we provide a list and pick the
    first one that matches.

--- init/windows-config.el
+++ init/windows-config.el
@@ -1,6 +1,10 @@
 ;; Configuration options specific to Windows
 
-(custom-set-faces
- '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
+(require 'cl)
+(defun font-candidate (&rest fonts)
+  "Return existing font which first match."
+  (find-if (lambda (f) (find-font (font-spec :name f))) fonts))
+
+(set-face-attribute 'default nil :font (font-candidate "Consolas-10:weight=normal" "Lucida Console-11:weight=normal"))
 
 (provide 'windows-config)
